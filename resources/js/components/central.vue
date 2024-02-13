bb5242148c2ee7246b76443dd679888f5d95ac80
Dzonatans <Dzonatan2017@gmail.com>
2024-02-13 16:57:53 +0200

    UI update

--- resources/js/components/central.vue
+++ resources/js/components/central.vue
@@ -4,24 +4,97 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="text-white dark:bg-gray-800 rounded-md text-3xl p-5 flex ">
                 <div class="flex-auto font-bold ">Tasks</div>
-                <div class=" bg-green-500 p-2 rounded-md"><button @click="CreateTask">New Task</button></div>
+                <div class=" bg-indigo-700 p-2 rounded-md px-4 py-2 hover:bg-indigo-800"><button @click="openModal">New Task</button></div>
             </div>
         </div>
     </div>
 
 
 
+    <div class="flex items-center justify-center">
+  </div>
+  <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" @close="closeModal" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black/25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-gray-800 p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6 text-white"
+              >
+                Adding a new task
+              </DialogTitle>
+              <div class="mt-2">
+                <p class="text-sm text-gray-500">
+                    ...
+                </p>
+              </div>
+
+              <div class="mt-4">
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-indigo-700 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                  @click="closeModal"
+                >
+                  Add
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+
 
 </template>
-<script>
-export default {
-    methods:{
-        CreateTask(){
-            $('#default-modal').modal('show')
-        }
-    }
+
+<script setup>
+import { ref } from 'vue'
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/vue'
+
+const isOpen = ref(false)
+
+function closeModal() {
+  isOpen.value = false
 }
-</script >
+function openModal() {
+  isOpen.value = true
+}
+</script>
+
+
 <style lang="">
 
 </style>
