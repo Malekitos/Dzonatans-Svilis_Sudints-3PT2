63f43f5df482f7926d76e36db2cb843f81bda23f
Dzonatans <Dzonatan2017@gmail.com>
2024-02-21 23:39:33 +0200

    update

--- database/migrations/2024_02_13_111035_create_tasks_table.php
+++ database/migrations/2024_02_13_111035_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('grupa');
-            $table->string('owner');
+            //$table->string('owner');
             $table->string('title');
             $table->string('date');
             $table->string('time');
